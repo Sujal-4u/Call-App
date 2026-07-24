403379f54f77b91a61e4fa471e2f5fde2798407c
Sujal R Kale <kalesujal6325@gmail.com>
2026-07-24 19:57:05 +0530

    started js

--- Script.js
+++ Script.js
@@ -1,1 +1,9 @@
+let closeForm = document.querySelector("#closeBtn");
+let create = document.querySelector("#creatBtn");
+let form = document.querySelector(".form-container");
 
+
+
+closeForm.addEventListener("click", function(){
+  form.style.display = "none";
+});
